f38f637577123edc70775e21eb5d2e3ffd6eba0c
NZ <popobomo@outlook.com>
2020-06-13 09:27:20 +0800

    scroll

---
+++ scorllTo.js
@@ -0,0 +1,16 @@
+export function scorllTo(dom, end, duration = 300) {
+  const start = dom.scrollTop
+  let startTime = Date.now()
+  return new Promise((resolve, reject) => {
+    ;(function animate() {
+      let delta = Math.min(duration, Date.now() - startTime)
+      let value = (delta / duration) * (end - start) + start
+      dom.scrollTop = value
+      if (delta < duration) {
+        requestAnimationFrame(animate)
+      } else {
+        resolve()
+      }
+    })()
+  })
+}
