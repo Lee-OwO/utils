5e471d620723573fb0a631a7cf42c7fb5cff6d72
lee <popobomo@outlook.com>
2020-06-20 21:03:17 +0800

    event

---
+++ event.js
@@ -0,0 +1,37 @@
+class Event {
+  constructor() {
+    this.clientList = {};
+  }
+  listen(key, fn) {
+    if (!this.clientList[key]) {
+      this.clientList[key] = [];
+    }
+    this.clientList[key].push(fn);
+  }
+  remove(key, fn) {
+    const fns = this.clientList[key];
+    if (!fns) {
+      return false;
+    }
+    if (!fn) {
+      fns && (fns.length = 0);
+    } else {
+      for (let i = fns.length - 1; i >= 0; i--) {
+        const currentFn = fns[i];
+        if (currentFn === fn) {
+          fns.splice(i, 1);
+        }
+      }
+    }
+  }
+  trigger() {
+    const key = [].shift.call(arguments);
+    const fns = this.clientList[key];
+    if (!fns || fns.length === 0) {
+      return false;
+    }
+    fns.forEach((fn) => {
+      fn.apply(this, arguments);
+    });
+  }
+}
